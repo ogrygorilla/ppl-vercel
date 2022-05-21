fec1fdb98f615ed7a80d3484a4b7dc7c37735a14
Oleksii Grygorchuk <o.grygorchuk@teravolt.tv>
2022-05-21 12:41:27 +0200

    add():firebase ui + hosting config updates

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -92,13 +92,15 @@ function SignInButtons() {
 // Sign out button
 function SignOutButton() {
   return (
-    <button
-      type="button"
-      className="m-auto inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      onClick={() => auth.signOut()}
-    >
-      Ausloggen
-    </button>
+    <div>
+      <button
+        type="button"
+        className="m-auto inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={() => auth.signOut()}
+      >
+        Ausloggen
+      </button>
+    </div>
   );
   // return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
