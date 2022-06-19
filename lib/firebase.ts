79b805f186c3caf16790def707bcb113bf6d0525
Oleksii Grygorchuk <o.grygorchuk@teravolt.tv>
2022-06-19 14:47:37 +0200

    update(): firebase + gitignore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,25 +1,17 @@
-import firebase from 'firebase/compat/app'
-import {getAuth} from 'firebase/auth'
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import firebase from "firebase/compat/app";
+import { getAuth } from "firebase/auth";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+
+import firebaseConfig  from "./firebaseConfig";
 
 /**
  * Do not check into source control
  * Should be in a .env file?
  */
-const firebaseConfig ={
-    apiKey: "AIzaSyDBScsxlr-BSh_6StoQ5mA7pomJglHyrOU",
-    authDomain: "populend-backend.firebaseapp.com",
-    projectId: "populend-backend",
-    storageBucket: "populend-backend.appspot.com",
-    messagingSenderId: "586260756742",
-    appId: "1:586260756742:web:bc1052c72bf1a015d34687",
-    measurementId: "G-V2D343TDGY"
-}
-
-if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig)
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
 //why 2 auth variables?
@@ -39,23 +31,23 @@ export const increment = firebase.firestore.FieldValue.increment;
  * Gets a users/{uid} document with username
  * @param  {string} username
  */
- export async function getUserWithUsername(username) {
-    const usersRef = firestore.collection('users');
-    const query = usersRef.where('username', '==', username).limit(1);
-    const userDoc = (await query.get()).docs[0];
-    return userDoc;
-  }
-  
-  /**`
-   * Converts a firestore document to JSON
-   * @param  {DocumentSnapshot} doc
-   */
-  export function promiseToJSON(doc) {
-    const data = doc.data();
-    return {
-      ...data,
-      // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
-      createdAt: data.createdAt.toMillis(),
-      updatedAt: data.updatedAt.toMillis(),
-    };
-  }
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
+ */
+export function promiseToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  };
+}
