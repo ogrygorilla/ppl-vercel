343ef6153722efd79cac25a0d28002cd07669cdb
Oleksii Grygorchuk <o.grygorchuk@teravolt.tv>
2022-09-11 14:48:15 +0200

    fix(): firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,8 +4,19 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
-import firebaseConfig  from "./firebaseConfig";
+// import firebaseConfig  from "./firebaseConfig";
 
+const firebaseConfig = {
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_MESSENGER_SENDER_ID}`,
+  appId: `${process.env.FIREBASE_APP_ID}`,
+  measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+};
+
+console.log("firebase config: ", firebaseConfig, process);
 /**
  * Do not check into source control
  * Should be in a .env file?
